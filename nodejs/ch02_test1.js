24c64ac23ad0edb7b697816846d2f51b6c8f6bcc
Hongsomang <ghdthakd12@naver.com>
2017-07-22 14:55:19 +0900

    .

--- nodejs/ch02_test1.js
+++ nodejs/ch02_test1.js
@@ -5,3 +5,8 @@ for(var i=1;i<=100;i++){
 }
 console.timeEnd('test');
 console.log('1부터 100까지 더한 결과물:%d',result);
+
+var Person={name:'소녀시대',age:18};
+console.dir(Person);
+console.log('현재 실행한 파일 이름:%s',__filename);
+console.log('현재 실행한 파일의 패스:%s',__dirname);
