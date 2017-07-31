bd3dc7a1839c77c3205f5121117b96d811156b76
Hongsomang <ghdthakd12@naver.com>
2017-07-31 20:54:08 +0900

    .

--- nodejs/ch02_test2.js
+++ nodejs/ch02_test2.js
@@ -1,2 +1,8 @@
-var Person={name:'소녀시대',age:18};
-console.dir(Person);
+console.log('argv 속성의 파라미터 수:'+process.argv.length);
+console.dir(process.argv);
+if(process.argv.length>2){
+    console.log('세 번째 피라미터 값:%s',process.argv[2])
+}
+process.argv.forEach(function(item,index){
+     console.log(index+';',item);
+});
