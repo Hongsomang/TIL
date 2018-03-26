33143cbdb0cc5afb8e6badde788e2a2aa45b4c90
Hongsomang <ghdthakd12@naver.com>
2018-03-26 23:43:07 +0900

    .

---
+++ java/chapter2/src/변수/ex2_2.java
@@ -0,0 +1,18 @@
+package º¯¼ö;
+
+public class ex2_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=10;
+		int y=20;
+		int tmp=0;
+		System.out.println("x:"+x+"y:"+y);
+		tmp=x;
+		x=y;
+		y=tmp;
+		System.out.println("x:"+x+"y:"+y);
+		
+	}
+
+}
